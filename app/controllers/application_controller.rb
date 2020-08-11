0c548a9f875a787e2d71700259562444e6159950
Kohei Yamamoto <kymmt90@gmail.com>
2020-08-11 16:36:35 +0900

    Sign in/out as a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:current_user_id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,33 @@
 class SessionsController < ApplicationController
+  def new
+    @user = current_user
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.authenticate(params[:password])
+      signin(user)
+    end
+
+    redirect_to new_session_path
+  end
+
+  def destroy
+    signout(current_user) if current_user
+
+    redirect_to new_session_path
+  end
+
+  private
+
+  def signin(user)
+    reset_session
+    session[:current_user_id] = user.id
+  end
+
+  def signout(user)
+    reset_session
+    @current_user = nil
+  end
 end
