0cd83ce92afc5a2d70a20b29e5d4dae523dea60e
Kohei Yamamoto <kymmt90@gmail.com>
2020-08-11 22:58:13 +0900

    Configure OAuth scopes

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -205,12 +205,9 @@ Doorkeeper.configure do
   #
   # enable_application_owner confirmation: false
 
-  # Define access token scopes for your provider
-  # For more information go to
-  # https://doorkeeper.gitbook.io/guides/ruby-on-rails/scopes
-  #
-  # default_scopes  :public
-  # optional_scopes :write, :update
+  default_scopes :read
+  optional_scopes :write
+  enforce_configured_scopes
 
   # Allows to restrict only certain scopes for grant_type.
   # By default, all the scopes will be available for all the grant types.
