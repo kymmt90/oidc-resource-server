7814e36cbf4b011d344600c5bca63f288dc946ea
Kohei Yamamoto <kymmt90@gmail.com>
2020-08-11 15:48:55 +0900

    Add constraints to items

--- db/migrate/20200811064759_create_items.rb
+++ db/migrate/20200811064759_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :title
+      t.string :title, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
